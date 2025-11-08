6be77f6abde85ebc3a33a9f5a68b0483fb8af347
Kiran Kolli <kiran.koli@gmail.com>
2025-11-08 19:59:31 +0000

    chore: add initial Copilot instructions and setup testcontainers for integration testing

--- src/main/java/com/kiran/musicapi/MusicapiApplication.java
+++ src/main/java/com/kiran/musicapi/MusicapiApplication.java
@@ -2,8 +2,13 @@ package com.kiran.musicapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class
+		}
+)
 public class MusicapiApplication {
 
 	public static void main(String[] args) {
